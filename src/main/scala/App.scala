fdb53e9b25ea3ab831b99d28d128ee8ee40aa807
ricardo-farias <ricardo.farias@thoughtworks.com>
2020-05-22 15:25:44 -0500

    [Ricardo] Added Test data. Completed project dependency setup.

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -4,8 +4,16 @@ import org.apache.spark.sql._
 object App {
 
   def main(args : Array[String]): Unit ={
-    val config = new SparkConf().setMaster("local").setAppName("ExcelParser")
-    val sparkContext = new SparkContext(config)
+    val config = new SparkConf().setMaster("local").setAppName("Practice")
+    val sc = new SparkContext(config)
+    val sql = SparkSession.builder().appName("Practice").master("local").getOrCreate()
+
+
+    val df = sql.read.format("csv")
+        .options(Map("inferSchema"->"true", "header"-> "true", "dateFormat"->"DD-MM-YY"))
+        .load("resource/TestData.csv")
+    df.show()
+
 
 
 
