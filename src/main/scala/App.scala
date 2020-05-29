5623f942813c96ebb79e8e5baee6da002d5b6e85
ricardo-farias <ricardo.farias@thoughtworks.com>
2020-05-29 15:34:02 -0500

    [Ricardo] Adds function that creates one dataframe of corrupted records, and another with noncorrupted records. Adds schema creation from json files.

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,42 +1,53 @@
 import org.apache.spark._
 import org.apache.spark.sql._
-import org.apache.spark.sql.types.{DateType, IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.catalyst.parser.LegacyTypeStringParser
+import org.apache.spark.sql.types.{DataType, DateType, IntegerType, StringType, StructField, StructType}
+
+import scala.io.{BufferedSource, Source}
+import scala.util.Try
 
 object App {
+
+  def goodAndBad(schema: StructType, filename: String)(implicit sparkSession: SparkSession) : (DataFrame, DataFrame) = {
+    val df = sparkSession.read.format("csv")
+      .options(
+        Map(
+          "header"-> "true",
+          "dateFormat"-> "MM/dd/yyyy",
+          "nullValue"-> "NULL",
+          "ignoreTrailingWhiteSpace"->"true",
+          "ignoreLeadingWhiteSpace"->"true",
+          "columnNameOfCorruptRecord"->"corrupted"
+        ))
+      .schema(schema)
+      .load(s"resource/${filename}.csv")
+
+    val badDF = df.filter(df.col("corrupted").isNotNull).toDF
+    val goodDF = df.filter(df.col("corrupted").isNull).toDF
+    (goodDF, badDF)
+  }
+
+  def readSchemaFromJson() : StructType = {
+    val source: BufferedSource = Source.fromFile("resource/TestDataSchema.json")
+    val data = source.getLines.toList.mkString("\n")
+    source.close()
+    val schema  = Try(DataType.fromJson(data)).getOrElse(LegacyTypeStringParser.parse(data)) match {
+      case t: StructType => t
+      case _             => throw new RuntimeException(s"Failed parsing StructType: $data")
+    }
+    schema
+  }
 
   def main(args : Array[String]): Unit ={
     val config = new SparkConf().setMaster("local").setAppName("Practice")
     val sc = new SparkContext(config)
     val sql = SparkSession.builder().appName("Practice").master("local").getOrCreate()
 
-    val schema = StructType(List(
-      StructField("City",StringType, false),
-      StructField("Population", IntegerType, false),
-      StructField("Date Collected", DateType, true),
-      StructField("_corrupt_record", StringType, true)
-    ))
+    val schema : StructType = readSchemaFromJson()
 
-    val df = sql.read.format("csv")
-        .options(
-          Map(
-            "header"-> "true",
-            "dateFormat"-> "MM/dd/yyyy",
-            "nullValue"-> "NULL",
-            "ignoreTrailingWhiteSpace"->"true",
-            "ignoreLeadingWhiteSpace"->"true"
-          ))
-        .schema(schema)
-        .load("resource/TestData.csv")
-    df.printSchema()
-
-
-    // Corrupted values can be found here
-    val badRows = df.filter(df.col("_corrupt_record").isNotNull)
-    badRows.collect().foreach(row => println(row))
-
-
-
-
+    val result = goodAndBad(schema, "TestData")(sql)
+    result._1.show
+    result._2.show
 
   }
 
