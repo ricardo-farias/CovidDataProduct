8b88c91d5e6073cd70465d3666cccfa76b5edcf2
ricardo-farias <ricardo.farias@thoughtworks.com>
2020-10-12 21:52:21 -0500

    [Ricardo][#81] Updating read and write code to use the new s3 folder structure

--- src/main/scala/com/ricardo/farias/App.scala
+++ src/main/scala/com/ricardo/farias/App.scala
@@ -2,7 +2,6 @@ package com.ricardo.farias
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.StructType
 
 object App {
 
@@ -22,71 +21,55 @@ object App {
     val fileStorage : FileSystem = if (Constants.env == "dev") LocalFileSystem
     else {
       sparkSession.sparkContext.hadoopConfiguration.set("spark.sql.sources.partitionOverwriteMode", "dynamic")
-      sparkSession.sparkContext.hadoopConfiguration.set("fs.s3a.endpoint", "s3.amazonaws.com")
       sparkSession.sparkContext.hadoopConfiguration.set("hive.metastore.connect.retries", "5")
       sparkSession.sparkContext.hadoopConfiguration.set("hive.metastore.client.factory.class",
         "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory")
       S3FileSystem
     }
 
-    fileStorage.listObjects()
-
-    val schema : StructType = fileStorage.readSchemaFromJson("raw", "TestDataSchema.json")(sparkSession.sparkContext)
-    println(schema)
-
-    val csvResult = fileStorage.readCsv(schema, "raw","TestData.csv")(sparkSession)
-    csvResult._1.show
-    csvResult._2.show
-    fileStorage.write("canonical","TestDataFromCsv", csvResult._1)
-
-    val jsonResult = fileStorage.readJson(schema,"raw", "TestData.json")(sparkSession)
-    jsonResult._1.show()
-    jsonResult._2.foreach(row => println(row.get(3)))
-    fileStorage.write("canonical","TestDataFromJson", jsonResult._1)
-
-    val italyProvinceSchema = fileStorage.readSchemaFromJson("raw","covid-italy/covid19-italy-province-schema")(sparkSession.sparkContext)
-    val covidItalyProvinceResults = fileStorage.readCsv(italyProvinceSchema, "raw", "covid-italy/covid19_italy_province.csv")(sparkSession)
+    val italyProvinceSchema = fileStorage.readSchemaFromJson("raw", "italy","covid-italy/covid19-italy-province-schema")(sparkSession.sparkContext)
+    val covidItalyProvinceResults = fileStorage.readCsv(italyProvinceSchema, "raw", "italy","covid-italy/covid19_italy_province.csv")(sparkSession)
     val covidItalyProvince = covidItalyProvinceResults._1
     covidItalyProvince.show()
     val corruptCovidItalyProvince = covidItalyProvinceResults._2
     corruptCovidItalyProvince.show()
-    fileStorage.write("canonical","covid-italy/covid19_italy_province", covidItalyProvince)
-    fileStorage.write("error","covid-italy/covid19_italy_province_err", corruptCovidItalyProvince)
+    fileStorage.write("canonical","italy", "covid-italy/covid19_italy_province", covidItalyProvince)
+    fileStorage.write("error", "italy","covid-italy/covid19_italy_province_err", corruptCovidItalyProvince)
 
-    val italyRegionSchema = fileStorage.readSchemaFromJson("raw","covid-italy/covid19-italy-region-schema.json")(sparkSession.sparkContext)
-    val covidItalyRegionResults = fileStorage.readCsv(italyRegionSchema,"raw", "covid-italy/covid19_italy_region.csv")(sparkSession)
+    val italyRegionSchema = fileStorage.readSchemaFromJson("raw", "italy", "covid-italy/covid19-italy-region-schema.json")(sparkSession.sparkContext)
+    val covidItalyRegionResults = fileStorage.readCsv(italyRegionSchema,"raw", "italy", "covid-italy/covid19_italy_region.csv")(sparkSession)
     val covidItalyRegion = covidItalyRegionResults._1
     covidItalyRegion.show()
     val corruptCovidItalyRegion = covidItalyRegionResults._2
     corruptCovidItalyRegion.show()
-    fileStorage.write("canonical","covid-italy/covid19_italy_region", covidItalyRegion)
-    fileStorage.write("error","covid-italy/covid19_italy_region_err", corruptCovidItalyRegion)
+    fileStorage.write("canonical", "italy","covid-italy/covid19_italy_region", covidItalyRegion)
+    fileStorage.write("error", "italy","covid-italy/covid19_italy_region_err", corruptCovidItalyRegion)
 
-    val usSchema = fileStorage.readSchemaFromJson("raw","covid-us/us-schema.json")(sparkSession.sparkContext)
-    val covidUsResults = fileStorage.readCsv(usSchema, "raw","covid-us/us.csv")(sparkSession)
+    val usSchema = fileStorage.readSchemaFromJson("raw", "us","covid-us/us-schema.json")(sparkSession.sparkContext)
+    val covidUsResults = fileStorage.readCsv(usSchema, "raw", "us","covid-us/us.csv")(sparkSession)
     val covidUs = covidUsResults._1
     covidUs.show()
     val corruptCovidUS = covidUsResults._2
     corruptCovidUS.show()
-    fileStorage.write("canonical", "covid-us/covid_us", covidUs)(sparkSession)
-    fileStorage.write("error", "covid-us/covid_us_err", covidUs)(sparkSession)
+    fileStorage.write("canonical", "us", "covid-us/covid_us", covidUs)(sparkSession)
+    fileStorage.write("error", "us", "covid-us/covid_us_err", covidUs)(sparkSession)
 
-    val usCountriesSchema = fileStorage.readSchemaFromJson("raw","covid-us/us-counties-schema.json")(sparkSession.sparkContext)
-    val covidUsCountriesResults = fileStorage.readCsv(usCountriesSchema, "raw","covid-us/us-counties.csv")(sparkSession)
+    val usCountriesSchema = fileStorage.readSchemaFromJson("raw", "us","covid-us/us-counties-schema.json")(sparkSession.sparkContext)
+    val covidUsCountriesResults = fileStorage.readCsv(usCountriesSchema, "raw", "us","covid-us/us-counties.csv")(sparkSession)
     val covidUsCountries = covidUsCountriesResults._1
     covidUsCountries.show()
     val corruptCovidUsCountries = covidUsCountriesResults._2
     corruptCovidUsCountries.show()
-    fileStorage.write("canonical","covid-us/covid_us_counties", covidUsCountries)(sparkSession)
-    fileStorage.write("error","covid-us/covid_us_counties_err", corruptCovidUsCountries)(sparkSession)
+    fileStorage.write("canonical", "us","covid-us/covid_us_counties", covidUsCountries)(sparkSession)
+    fileStorage.write("error", "us","covid-us/covid_us_counties_err", corruptCovidUsCountries)(sparkSession)
 
-    val usStatesSchema = fileStorage.readSchemaFromJson("raw","covid-us/us-states-schema.json")(sparkSession.sparkContext)
-    val covidUsStatesResults = fileStorage.readCsv(usStatesSchema, "raw", "covid-us/us-states.csv")(sparkSession)
+    val usStatesSchema = fileStorage.readSchemaFromJson("raw", "us","covid-us/us-states-schema.json")(sparkSession.sparkContext)
+    val covidUsStatesResults = fileStorage.readCsv(usStatesSchema, "raw", "us", "covid-us/us-states.csv")(sparkSession)
     val covidUsStates = covidUsStatesResults._1
     covidUsStates.show()
     val corruptCovidUsStates = covidUsStatesResults._2
     corruptCovidUsStates.show()
-    fileStorage.write("canonical","covid-us/covid_us_states", covidUsStates)(sparkSession)
-    fileStorage.write("error","covid-us/covid_us_states", corruptCovidUsStates)(sparkSession)
+    fileStorage.write("canonical", "us","covid-us/covid_us_states", covidUsStates)(sparkSession)
+    fileStorage.write("error", "us","covid-us/covid_us_states", corruptCovidUsStates)(sparkSession)
   }
 }
--- src/main/scala/com/ricardo/farias/FileSystem.scala
+++ src/main/scala/com/ricardo/farias/FileSystem.scala
@@ -11,10 +11,10 @@ import scala.io.{BufferedSource, Source}
 import scala.util.Try
 
 abstract class FileSystem {
-  def readJson(schema: StructType, filePath: String, filename: String)(implicit sparkSession: SparkSession) : (DataFrame, DataFrame)
-  def readCsv(schema: StructType, filePath: String, filename: String)(implicit sparkSession: SparkSession) : (DataFrame, DataFrame)
-  def readSchemaFromJson(filePath: String, filename: String)(implicit sparkContext: SparkContext) : StructType
-  def write(filePath: String, filename: String, data: DataFrame)(implicit sparkSession: SparkSession) : Unit
+  def readJson(schema: StructType, filePath: String, product: String, filename: String, timeStampFormat: String = "MM/dd/yyyy hh:mm:ss a")(implicit sparkSession: SparkSession) : (DataFrame, DataFrame)
+  def readCsv(schema: StructType, filePath: String, product: String, filename: String, timeStampFormat: String = "MM/dd/yyyy hh:mm:ss a")(implicit sparkSession: SparkSession) : (DataFrame, DataFrame)
+  def readSchemaFromJson(filePath: String, product: String, filename: String)(implicit sparkContext: SparkContext) : StructType
+  def write(filePath: String, product: String, filename: String, data: DataFrame)(implicit sparkSession: SparkSession) : Unit
   def listObjects() : Unit
 }
 
@@ -22,39 +22,41 @@ object LocalFileSystem extends FileSystem {
 
   var ROOT_DIRECTORY = Constants.directory
 
-  override def readJson(schema: StructType, filePath: String, filename: String)(implicit sparkSession: SparkSession): (DataFrame, DataFrame) = {
+  override def readJson(schema: StructType, filePath: String, product: String, filename: String, timeStampFormat: String)(implicit sparkSession: SparkSession): (DataFrame, DataFrame) = {
     val df = sparkSession.read.options(
       Map("dateFormat"->"MM/dd/yy",
         "columnNameOfCorruptRecord"->"Corrupted",
+        "timestampFormat"->timeStampFormat,
         "nullValues"->"NULL"))
       .schema(schema)
-      .json(f"${ROOT_DIRECTORY}/${filePath}/${filename}")
+      .json(f"${ROOT_DIRECTORY}/${product}/${filePath}/${filename}")
     val badDF = df.filter(df.col("Corrupted").isNotNull).toDF
     val goodDF = df.filter(df.col("Corrupted").isNull).toDF
     (goodDF, badDF)
   }
 
-  override def readCsv(schema: StructType, filePath: String, filename: String)(implicit sparkSession: SparkSession) : (DataFrame, DataFrame) = {
+  override def readCsv(schema: StructType, filePath: String, product: String, filename: String, timeStampFormat: String)(implicit sparkSession: SparkSession) : (DataFrame, DataFrame) = {
     val df = sparkSession.read.format("csv")
       .options(
         Map(
           "header"-> "true",
           "dateFormat"-> "MM/dd/yyyy",
+          "timestampFormat"->timeStampFormat,
           "nullValue"-> "NULL",
           "ignoreTrailingWhiteSpace"->"true",
           "ignoreLeadingWhiteSpace"->"true",
           "columnNameOfCorruptRecord"->"Corrupted"
         ))
       .schema(schema)
-      .load(s"${ROOT_DIRECTORY}/${filePath}/${filename}")
+      .load(s"${ROOT_DIRECTORY}/${product}/${filePath}/${filename}")
 
     val badDF = df.filter(df.col("Corrupted").isNotNull).toDF
     val goodDF = df.filter(df.col("Corrupted").isNull).toDF
     (goodDF, badDF)
   }
 
-  override def readSchemaFromJson(filePath: String, filename: String)(implicit sparkContext: SparkContext) : StructType = {
-    val source: BufferedSource = Source.fromFile(s"${ROOT_DIRECTORY}/${filePath}/${filename}")
+  override def readSchemaFromJson(filePath: String, product: String, filename: String)(implicit sparkContext: SparkContext) : StructType = {
+    val source: BufferedSource = Source.fromFile(s"${ROOT_DIRECTORY}/${product}/${filePath}/${filename}")
     val data = source.getLines.toList.mkString("\n")
     source.close()
     val schema  = Try(DataType.fromJson(data)).getOrElse(LegacyTypeStringParser.parse(data)) match {
@@ -66,8 +68,8 @@ object LocalFileSystem extends FileSystem {
 
   def setRootDirectory(directory: String) : Unit = ROOT_DIRECTORY = directory
 
-  override def write(filePath: String, filename: String, data : DataFrame)(implicit sparkSession: SparkSession)  : Unit = {
-    data.write.mode(SaveMode.Overwrite).parquet(f"${ROOT_DIRECTORY}/${filePath}/${filename}.parquet")
+  override def write(filePath: String, product: String, filename: String, data : DataFrame)(implicit sparkSession: SparkSession)  : Unit = {
+    data.write.mode(SaveMode.Overwrite).parquet(f"${ROOT_DIRECTORY}/${product}/${filePath}/${filename}.parquet")
   }
 
   override def listObjects() : Unit = {
@@ -80,40 +82,40 @@ object LocalFileSystem extends FileSystem {
 object S3FileSystem extends FileSystem {
   private val bucket = Constants.bucket
 
-  override def readCsv(schema : StructType, filePath: String, filename: String)(implicit sparkSession: SparkSession) : (DataFrame, DataFrame) = {
+  override def readCsv(schema : StructType, filePath: String, product: String, filename: String, timeStampFormat: String)(implicit sparkSession: SparkSession) : (DataFrame, DataFrame) = {
     val df = sparkSession.read.format("csv")
       .options(
         Map(
           "header"-> "true",
           "dateFormat"-> "MM/dd/yyyy",
-          "timestampFormat"->"MM/dd/yyyy hh:mm:ss a",
+          "timestampFormat"->timeStampFormat,
           "nullValue"-> "NULL",
           "ignoreTrailingWhiteSpace"->"true",
           "ignoreLeadingWhiteSpace"->"true",
           "columnNameOfCorruptRecord"->"Corrupted"
         ))
       .schema(schema)
-      .load(f"s3a://${bucket}/${filePath}/${filename}")
+      .load(f"s3a://${bucket}/${product}/${filePath}/${filename}")
     val badDF = df.filter(df.col("Corrupted").isNotNull).toDF
     val goodDF = df.filter(df.col("Corrupted").isNull).toDF
     (goodDF, badDF)
   }
 
-  override def readJson(schema: StructType, filePath: String, filename: String)(implicit sparkSession: SparkSession) : (DataFrame, DataFrame) = {
+  override def readJson(schema: StructType, filePath: String, product: String, filename: String, timeStampFormat: String)(implicit sparkSession: SparkSession) : (DataFrame, DataFrame) = {
     val df = sparkSession.read.options(
       Map("dateFormat"->"MM/dd/yy",
-        "timestampFormat"->"MM/dd/yyyy hh:mm:ss a",
+        "timestampFormat"->timeStampFormat,
         "columnNameOfCorruptRecord"->"Corrupted",
         "nullValues"->"NULL"))
       .schema(schema)
-      .json(f"s3a://${bucket}/${filePath}/${filename}")
+      .json(f"s3a://${bucket}/${product}/${filePath}/${filename}")
     val badDF = df.filter(df.col("Corrupted").isNotNull).toDF
     val goodDF = df.filter(df.col("Corrupted").isNull).toDF
     (goodDF, badDF)
   }
 
-  override def readSchemaFromJson(filePath: String, filename: String)(implicit sparkContext: SparkContext) : StructType = {
-    val source = sparkContext.textFile(f"s3a://${bucket}/${filePath}/${filename}").collect()
+  override def readSchemaFromJson(filePath: String, product: String, filename: String)(implicit sparkContext: SparkContext) : StructType = {
+    val source = sparkContext.textFile(f"s3a://${bucket}/${product}/${filePath}/${filename}").collect()
 
     val data = source.toList.mkString("\n")
     val schema  = Try(DataType.fromJson(data)).getOrElse(LegacyTypeStringParser.parse(data)) match {
@@ -123,7 +125,7 @@ object S3FileSystem extends FileSystem {
     schema
   }
 
-  override def write(filePath: String, filename: String, data: DataFrame)(implicit sparkSession: SparkSession) : Unit = {
+  override def write(filePath: String, product: String, filename: String, data: DataFrame)(implicit sparkSession: SparkSession) : Unit = {
     val name = if (filename.contains("/")) filename.split("/")(1) else filename
     data.createOrReplaceTempView(name)
 //    sparkSession.sql(s"""CREATE TABLE default.${name}
@@ -134,7 +136,7 @@ object S3FileSystem extends FileSystem {
     //data.write.mode(SaveMode.Overwrite).parquet(f"s3a://${bucket}/${filename}.parquet")
     data.write.format("parquet")
       .mode(SaveMode.Overwrite)
-      .option("path", f"s3a://${bucket}/${filePath}/${filename}.parquet")
+      .option("path", f"s3a://${bucket}/${product}/${filePath}/${filename}.parquet")
       .saveAsTable(s"${Constants.database}.${name}")
   }
 
